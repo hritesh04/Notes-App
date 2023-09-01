1beec1f54b7abb04be405b41194f91209b14ec80
Hritesh Mondal <134622021+hritesh04@users.noreply.github.com>
2023-09-01 09:15:13 +0530

    Delete src directory

--- src/assets/controller/handleNoteClick.js
+++
@@ -1,9 +0,0 @@
-const handleNoteClick = (navigate, notes, _id) => {
-  navigate(`/edit/${_id}`, {
-    state: {
-      _id: _id,
-      note: notes,
-    },
-  });
-};
-export default handleNoteClick;
